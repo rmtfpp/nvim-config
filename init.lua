1b5bca734db6b7b157e1586fc9a94f573e4b524c
Filippo Romiti <dev.rmtfpp@gmail.com>
2025-07-26 10:30:45 +0200

    before debian

--- init.lua
+++ init.lua
@@ -878,6 +878,30 @@ require('lazy').setup({
   },
 
   {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- optional, for file icons
+    config = function()
+      require('nvim-tree').setup {
+        view = {
+          width = 30,
+          side = 'left',
+        },
+        renderer = {
+          icons = {
+            show = {
+              file = true,
+              folder = true,
+            },
+          },
+        },
+        hijack_netrw = true, -- this disables netrw completely
+      }
+    end,
+
+    vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true }),
+  },
+
+  {
     'projekt0n/github-nvim-theme',
     name = 'github-theme',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
@@ -892,6 +916,37 @@ require('lazy').setup({
       }
 
       vim.cmd 'colorscheme github_light'
+    end,
+  },
+
+  {
+    'ej-shafran/compile-mode.nvim',
+    version = '^5.0.0',
+    -- you can just use the latest version:
+    -- branch = "latest",
+    -- or the most up-to-date updates:
+    -- branch = "nightly",
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- if you want to enable coloring of ANSI escape codes in
+      -- compilation output, add:
+      -- { "m00qek/baleia.nvim", tag = "v1.3.0" },
+    },
+    config = function()
+      ---@type CompileModeOpts
+      vim.g.compile_mode = {
+        -- to add ANSI escape code support, add:
+        -- baleia_setup = true,
+
+        -- to make `:Compile` replace special characters (e.g. `%`) in
+        -- the command (and behave more like `:!`), add:
+        -- bang_expansion = true,
+      }
+
+      -- remap keys
+      vim.keymap.set('n', '<leader>c', '<cmd>Compile<CR>', { desc = 'Compile current project' })
+      vim.keymap.set('n', '<leader>n', '<cmd>NextError<CR>', { desc = 'Go to next compile error' })
+      vim.keymap.set('n', '<leader>r', '<cmd>Recompile<CR>', { desc = 'Re-run last compilation' })
     end,
   },
 
